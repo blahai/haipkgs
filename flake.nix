20290dea1a4895285e68a1bae81271cac60577d9
blahai <github@blahai.gay>
2025-01-31 18:22:54 +0200

    init v2 && first package (JKPS)

--- flake.nix
+++ flake.nix
@@ -1,33 +1,52 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    hjem = {
+      url = "github:feel-co/hjem";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    systems,
+    hjem,
     ...
   }: let
-    inherit (nixpkgs) lib;
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs (import systems) (system: function nixpkgs.legacyPackages.${system});
 
-    forAllSystems = fn:
-      lib.genAttrs lib.systems.flakeExposed (
-        system:
-          fn (
-            import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            }
-          )
-      );
+    loadPackages = pkgs: let
+      packageNames = builtins.attrNames (builtins.readDir ./pkgs);
+    in
+      nixpkgs.lib.genAttrs packageNames (name: pkgs.callPackage (./pkgs + "/${name}") {});
+
+    loadModules = dir: let
+      moduleNames = builtins.attrNames (builtins.readDir dir);
+    in
+      nixpkgs.lib.genAttrs moduleNames (name: import (dir + "/${name}"));
+
+    loadOverlays = let
+      overlayNames = builtins.attrNames (builtins.readDir ./overlays);
+    in
+      nixpkgs.lib.genAttrs overlayNames (name: import (./overlays + "/${name}"));
   in {
-    formatter = forAllSystems (pkgs: pkgs.alejandra);
-    overlays.default = _: prev: self.packages.${prev.stdenv.hostPlatform.system} or {};
+    packages = forAllSystems loadPackages;
+
+    nixosModules = loadModules ./modules/nixos;
+    hjemModules = loadModules ./modules/hjem;
+
+    overlays = loadOverlays;
   };
+
   nixConfig = {
-    extra-substituters = ["https://blahai.cachix.org"];
+    extra-substituters = ["https://blahai.cachix.org" "https://nix-community.cachix.org/"];
     extra-trusted-public-keys = [
       "blahai.cachix.org-1:7Pg+JDWpcIogeN3HesRnOb4dl6FSwl/ZHC+QUmb65So="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
