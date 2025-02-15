da195db4f85213bd26bd8d9484e12a30cd0a87ce
blahai <github@blahai.gay>
2025-02-15 19:04:38 +0200

    haiLib: add `ifTheyExist` and `mkProgram`

--- lib/default.nix
+++ lib/default.nix
@@ -1,3 +1,5 @@
 {lib}: {
   inherit (import ./services.nix {inherit lib;}) mkServiceOption;
+  inherit (import ./programs.nix {inherit lib;}) mkProgram;
+  inherit (import ./validators.nix {inherit lib;}) ifTheyExist;
 }
---
+++ lib/validators.nix
@@ -0,0 +1,10 @@
+{...}: let
+  inherit
+    (builtins)
+    filter
+    hasAttr
+    ;
+  ifTheyExist = config: groups: filter (group: hasAttr group config.users.groups) groups;
+in {
+  inherit ifTheyExist;
+}
